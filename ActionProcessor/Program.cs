482eac264728a0e7b9ebb9612bc3267de992deb0
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2025-07-11 14:38:21 -0400

    refactor: Update Swagger UI route prefix to 'swagger'

--- ActionProcessor/Program.cs
+++ ActionProcessor/Program.cs
@@ -93,7 +93,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Action Processor API v1");
-        c.RoutePrefix = string.Empty; // Serve Swagger UI at root
+        c.RoutePrefix = "swagger";
     });
 }
 
